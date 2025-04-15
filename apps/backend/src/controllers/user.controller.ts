e84a3178e3d27b46d57e4de67968513eac04d108
Naresh-official <naresh01.official@gmail.com>
2025-04-15 18:04:23 +0530

    - setup express server - user routes and controllers for getUser and createUser

---
+++ apps/backend/src/controllers/user.controller.ts
@@ -0,0 +1,56 @@
+import prisma from "@workspace/db/*";
+import { Request, Response } from "express";
+
+export async function getUser(req: Request, res: Response) {
+  const { email } = req.params;
+  if (!email) {
+    res.status(400).json({ error: "User Email is required" });
+    return;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+    res.status(200).json(user);
+    return;
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    return;
+  }
+}
+
+export async function createUser(req: Request, res: Response) {
+  const { name, email, profilePicture } = req.body;
+  if (!name || !email) {
+    res.status(400).json({ error: "Name and email are required" });
+    return;
+  }
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      res.status(409).json({ error: "User already exists" });
+      return;
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        profilePicture,
+      },
+    });
+    res.status(201).json(user);
+    return;
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    return;
+  }
+}
---
+++ apps/backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { env } from "@workspace/config";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = parseInt(env.PORT);
+
+// Import routes
+import userRoutes from "./routes/user.routes";
+
+app.use("/api/v1/user", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
