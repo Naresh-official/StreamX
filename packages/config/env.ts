7e8587e47f5ba788b9597fde00ec6e5d319a87bd
Naresh-official <naresh01.official@gmail.com>
2025-04-15 17:59:52 +0530

    - update env.ts , index.ts files to read env files across the repo - add package.json file to make the package funcitonal

--- packages/config/env.ts
+++ packages/config/env.ts
@@ -1,10 +1,13 @@
-import { configDotenv } from "dotenv";
+import { config } from "dotenv";
+import { resolve } from "path";
 import z from "zod";
 
-configDotenv();
+config({ path: resolve(__dirname, "../db/.env") });
+config({ path: resolve(__dirname, "../../.env") });
 
 const envSchema = z.object({
-	DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url(),
+  PORT: z.string().default("8000"),
 });
 
 export const env = envSchema.parse(process.env);
