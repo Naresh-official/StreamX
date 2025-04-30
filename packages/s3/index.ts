a5c579a58c0fe5653246c0d76508b9bf8b918dda
Naresh-official <naresh01.official@gmail.com>
2025-04-30 18:42:42 +0530

    add: add function to generate presignedUrl and upload to presignedUrl

--- packages/s3/index.ts
+++ packages/s3/index.ts
@@ -5,6 +5,8 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import https from "https";
 
 const s3 = new S3Client({
   region: env.AWS_REGION,
@@ -23,7 +25,7 @@ export async function uploadToS3(filePath: string, key: string): Promise<void> {
 
     const command = new PutObjectCommand({
       Bucket: env.S3_BUCKET,
-      Key: "123/" + key,
+      Key: key,
       Body: fileBuffer,
     });
     await s3.send(command);
@@ -35,10 +37,9 @@ export async function uploadToS3(filePath: string, key: string): Promise<void> {
   }
 }
 
-// TODO: Implement downloadFromS3 function
 export async function downloadFromS3(
   videoKey: string,
-  filePath: string,
+  filePath: string
 ): Promise<void> {
   const command = new GetObjectCommand({
     Bucket: env.S3_BUCKET,
@@ -66,5 +67,52 @@ export async function downloadFromS3(
       console.error("Error writing file:", error);
       reject(error);
     });
+  });
+}
+
+export async function generatePresignedUrlForUpload(
+  videoKey: string,
+  expiresIn: number
+): Promise<string> {
+  const command = new PutObjectCommand({
+    Bucket: env.S3_BUCKET,
+    Key: videoKey,
+    ContentType: "video/mp4",
+  });
+  return getSignedUrl(s3, command, { expiresIn: expiresIn });
+}
+
+export async function uploadToPresignedUrl(
+  presignedUrl: string,
+  fileBuffer: Buffer
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const url = new URL(presignedUrl);
+
+    const options = {
+      method: "PUT",
+      hostname: url.hostname,
+      path: url.pathname + url.search,
+      headers: {
+        "Content-Length": fileBuffer.length,
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
+        console.log("Presigned upload successful");
+        resolve();
+      } else {
+        reject(
+          new Error(
+            `Presigned upload failed with status code ${res.statusCode}`
+          )
+        );
+      }
+    });
+
+    req.on("error", reject);
+    req.write(fileBuffer);
+    req.end();
   });
 }
