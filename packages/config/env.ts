4c9a1fbaabfca600dfb945a75304e87d8ac4d926
Naresh-official <naresh01.official@gmail.com>
2025-04-30 18:40:36 +0530

    add new env's for cloudinary

--- packages/config/env.ts
+++ packages/config/env.ts
@@ -25,6 +25,10 @@ const envSchema = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   NEXTAUTH_SECRET: z.string(),
+
+  CLOUDINARY_CLOUD_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_API_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
