32fbc49d2c99be44a500cc0cc0e3920abd103e69
Naresh-official <naresh01.official@gmail.com>
2025-05-09 20:31:54 +0530

    add : call backend api to update video status while processing and uploading

--- apps/transcoder/processor.ts
+++ apps/transcoder/processor.ts
@@ -7,6 +7,7 @@ import {
   ffmpegOutput480pOptions,
   ffmpegOutput720pOptions,
   ffmpegOutput1080pOptions,
+  env,
 } from "@workspace/config/server";
 
 async function transcodeResolution(
@@ -28,37 +29,61 @@ async function transcodeResolution(
 export async function processVideo(job: Job) {
   const { videoKey, videoId } = job.data;
   console.log(`Starting video transcoding job for ${videoId}...`);
-  const inputPath = `${process.cwd()}/tmp/${videoId}-input.mp4`;
 
-  await downloadFromS3(videoKey, inputPath);
+  await fetch(`${env.BACKEND_URL}/video/${videoId}/processing`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-  const output480p = `./tmp/480/index.m3u8`;
-  const output720p = `./tmp/720/index.m3u8`;
-  const output1080p = `./tmp/1080/index.m3u8`;
+  try {
+    const inputPath = `${process.cwd()}/tmp/${videoId}-input.mp4`;
+    await downloadFromS3(videoKey, inputPath);
 
-  if (!fs.existsSync(inputPath)) {
-    console.error(`Input file not found: ${inputPath}`);
-    throw new Error(`Input file not found: ${inputPath}`);
-  } else {
-    console.log(`Input file exists: ${inputPath}`);
-  }
+    const output480p = `./tmp/480/index.m3u8`;
+    const output720p = `./tmp/720/index.m3u8`;
+    const output1080p = `./tmp/1080/index.m3u8`;
 
-  await transcodeResolution(inputPath, output480p, ffmpegOutput480pOptions);
-  await transcodeResolution(inputPath, output720p, ffmpegOutput720pOptions);
-  await transcodeResolution(inputPath, output1080p, ffmpegOutput1080pOptions);
+    if (!fs.existsSync(inputPath)) {
+      console.error(`Input file not found: ${inputPath}`);
+      throw new Error(`Input file not found: ${inputPath}`);
+    } else {
+      console.log(`Input file exists: ${inputPath}`);
+    }
 
-  fs.unlinkSync(inputPath);
+    await transcodeResolution(inputPath, output480p, ffmpegOutput480pOptions);
+    await transcodeResolution(inputPath, output720p, ffmpegOutput720pOptions);
+    await transcodeResolution(inputPath, output1080p, ffmpegOutput1080pOptions);
 
-  for (const res of [480, 720, 1080]) {
-    const dir = `./tmp/${res}`;
-    for (const file of fs.readdirSync(dir)) {
-      const filePath = `${dir}/${file}`;
-      if (filePath.endsWith(".m3u8") || filePath.endsWith(".ts")) {
-        await uploadToS3(filePath, `${videoId}/${res}/${file}`);
-        fs.unlinkSync(filePath);
+    fs.unlinkSync(inputPath);
+
+    for (const res of [480, 720, 1080]) {
+      const dir = `./tmp/${res}`;
+      for (const file of fs.readdirSync(dir)) {
+        const filePath = `${dir}/${file}`;
+        if (filePath.endsWith(".m3u8") || filePath.endsWith(".ts")) {
+          await uploadToS3(filePath, `${videoId}/${res}/${file}`);
+          fs.unlinkSync(filePath);
+        }
       }
     }
-  }
 
-  console.log(`Transcoding completed for video ID: ${videoId}`);
+    await fetch(`${env.BACKEND_URL}/video/${videoId}/completed`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(`Transcoding completed for video ID: ${videoId}`);
+  } catch (error) {
+    await fetch(`${env.BACKEND_URL}/video/${videoId}/failed`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.error(`Error processing video ID ${videoId}:`, error);
+  }
 }
